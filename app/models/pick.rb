8b373bbbbdee456b17e0e16fdcd4067f4f19282a
Yuta <ezochi66@gmail.com>
2020-10-23 16:54:08 +0900

    try showing maos

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -1,4 +1,6 @@
 class Pick < ApplicationRecord
     belongs_to :group
     mount_uploader :picture, PictureUploader
+    geocoded_by :address
+    after_validation :geocode, if: :address_changed?
 end
--- db/migrate/20201020132454_add_address_to_picks.rb
+++ db/migrate/20201020132454_add_address_to_picks.rb
@@ -1,5 +1,8 @@
 class AddAddressToPicks < ActiveRecord::Migration[6.0]
   def change
     add_column :picks, :address, :string
+
+    add_column :picks, :latitude, :float
+    add_column :picks, :longitude, :float
   end
 end
